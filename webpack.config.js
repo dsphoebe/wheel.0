06903392da933cf51c147dbeb3e535dd0915a6ff
dsphoebe <syh1217@qq.com>
2019-03-18 15:09:15 +0800

    添加 webpack externals 配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: {
-    'wheel.0': './lib/index.tsx'
+    'wheel.react': './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
@@ -22,5 +25,19 @@ module.exports = {
       title: 'Wheel.0',
       template: 'index.html'
     })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'react'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
+  }
 }
