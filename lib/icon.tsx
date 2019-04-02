3bebe269f027537123eb1325cc5c1549048e4634
dsphoebe <syh1217@qq.com>
2019-04-02 17:51:21 +0800

    add onClick event

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,13 +4,14 @@ import './icon.scss';
 
 interface IconProps {
     name: string;
+    onClick: React.MouseEventHandler<SVGElement>;
 }
 
 // Icon 是一个 React.FunctionComponet 组件
 // 它有一个属性是 IconProps
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className={"wui-icon"}>
+        <svg className={"wui-icon"} onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`} />
         </svg>
     );
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import Icon from './icon';
 
 ReactDOM.render(<div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
-    <Icon name="qq" />
+    <Icon name="wechat" onClick={() => console.log('hahah')}/>
 </div>, document.getElementById('root'));
