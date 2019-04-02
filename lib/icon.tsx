a8d3aa5a372588bef1f5a000e82a21555a708e97
dsphoebe <syh1217@qq.com>
2019-04-03 00:42:15 +0800

    icon 添加事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import './importIcons';
 import './icon.scss';
 
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string;
-    onClick: React.MouseEventHandler<SVGElement>;
 }
 
 // Icon 是一个 React.FunctionComponet 组件
 // 它有一个属性是 IconProps
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className={"wui-icon"} onClick={props.onClick}>
+        <svg className={"wui-icon"} {...props}>
             <use xlinkHref={`#${props.name}`} />
         </svg>
     );
