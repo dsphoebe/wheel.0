e64662be89a345809bc4b287a71cea8140411229
dsphoebe <syh1217@qq.com>
2019-04-03 14:33:22 +0800

    发布到 npm

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-ReactDOM.render(<div>
-    <Icon name="wechat" onClick={() => console.log('hahah')}/>
-</div>, document.getElementById('root'));
+export { default as Icon } from './icon/icon';
